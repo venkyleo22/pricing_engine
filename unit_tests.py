896901b26ed44881f5a7038349118585d4c18aaa
venkatesh k <45560455+venkyleo22@users.noreply.github.com>
2020-07-25 14:31:16 +0530

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,4 +1,13 @@
 import unittest
+from models import wheel, handle, chain, seating, frame, cycle
 
-class Testframe((unittest.TestCase):
-    pass
+
+class Testframe(unittest.TestCase):
+    def test_fun1(self):
+        f = frame(2,25,5,50)
+        result = f.price()
+        assert result==200, "failed to calculate price" 
+
+
+if __name__ == "__main__":
+    unittest.main()
